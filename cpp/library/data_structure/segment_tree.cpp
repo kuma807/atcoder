9bf58ef7db7137ac11beda59023635d1706b1166
kuma807 <kumakura013@gmail.com>
2021-07-01 19:11:58 +0900

    update 2021/7/1

--- cpp/library/data_structure/segment_tree.cpp
+++ cpp/library/data_structure/segment_tree.cpp
@@ -1,6 +1,6 @@
 // ============how it works=================
-//void init(n_)
-  //ノード数n_の初期化
+//segment_tree seg(N);
+  //ノード数Nの初期化
 
 //MAX_N
   //最大ノード数
@@ -17,37 +17,55 @@
 // ========================================
 
 //=============segment_tree============================
-const int MAX_N = 1 << 17;//要変更(1 << 17 ≒ 10 ^ 5)
-int nn;
-vector<ll> dat(2 * MAX_N - 1, 0);//要変更
+struct segment_tree {
+  int MAX_N;
+  int nn;
+  vector<ll> dat;
+  segment_tree(ll n_) {
+    MAX_N = 1 << 17;//要変更(1 << 17 ≒ 10 ^ 5)
+    dat = vector<ll>(2 * MAX_N - 1, 0);//要変更
+    nn = 1;
+    while (nn < n_) {
+      nn *= 2;
+    }
+  }
 
-void init(ll n_) {
-  nn = 1;
-  while (nn < n_) {
-    nn *= 2;
+  void update(ll k, ll a) {
+    k += nn - 1;
+    dat.at(k) += a;//要変更
+    while (k > 0) {
+      k = (k - 1) / 2;
+      dat.at(k) = dat.at(k * 2 + 1) + dat.at(k * 2 + 2);//要変更
+    }
   }
-}
 
-void update(ll k, ll a) {
-  k += nn - 1;
-  dat.at(k) += a;//要変更
-  while (k > 0) {
-    k = (k - 1) / 2;
-    dat.at(k) = dat.at(k * 2 + 1) + dat.at(k * 2 + 2);//要変更
+  ll query(ll a, ll b, ll k, ll l, ll r) {
+    if (r <= a || b <= l) {
+      return 0;//要変更
+    }
+    if (a <= l && r <= b) {
+      return dat.at(k);
+    }
+    else {
+      ll vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+      ll vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+      return vl + vr;//要変更
+    }
   }
-}
 
-ll query(ll a, ll b, ll k = 0, ll l = 0, ll r = nn) {
-  if (r <= a || b <= l) {
-    return 0;//要変更
+  ll query(ll a, ll b) {
+    ll k = 0, l = 0, r = nn;
+    if (r <= a || b <= l) {
+      return 0;//要変更
+    }
+    if (a <= l && r <= b) {
+      return dat.at(k);
+    }
+    else {
+      ll vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+      ll vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+      return vl + vr;//要変更
+    }
   }
-  if (a <= l && r <= b) {
-    return dat.at(k);
-  }
-  else {
-    ll vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-    ll vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-    return vl + vr;//要変更
-  }
-}
+};
 //=================================================
---
+++ cpp/library/graph/SCC.cpp
@@ -0,0 +1,56 @@
+// ============need input=================
+// SCC scc(connection);
+//scc.cmp.at(i)
+  //iがどの強連結成分に属するか
+// ========================================
+
+//=============SCC============================
+struct SCC {
+  vector<vector<ll>> G;
+  vector<vector<ll>> rG;
+  vector<ll> vs;
+  vector<bool> used;
+  vector<ll> cmp;
+  SCC(vector<vector<ll>> &connection) {
+    G = connection;
+    rG = vector<vector<ll>>(connection.size());
+    used = vector<bool>(connection.size(), false);
+    cmp = vector<ll>(connection.size());
+    for (ll i = 0; i < connection.size(); ++i) {
+      for (ll j = 0; j < connection.at(i).size(); ++j) {
+        rG.at(connection.at(i).at(j)).push_back(i);
+      }
+    }
+    for (ll i = 0; i < connection.size(); ++i) {
+      if (!used.at(i)) {
+        dfs(i);
+      }
+    }
+    used = vector<bool>(connection.size(), false);
+    ll k = 0;
+    for (ll i = vs.size() - 1; i >= 0; --i) {
+      if (!used.at(vs.at(i))) {
+        rdfs(vs.at(i), k++);
+      }
+    }
+  }
+  void dfs(ll v) {
+    used.at(v) = true;
+    for (ll i = 0; i < G.at(v).size(); ++i) {
+      if (!used.at(G.at(v).at(i))) {
+        dfs(G.at(v).at(i));
+      }
+    }
+    vs.push_back(v);
+  }
+  void rdfs(ll v, ll k) {
+    used.at(v) = true;
+    cmp.at(v) = k;
+    for (ll i = 0; i < rG.at(v).size(); ++i) {
+      if (!used.at(rG.at(v).at(i))) {
+        rdfs(rG.at(v).at(i), k);
+      }
+    }
+  }
+};
+//=================================================
