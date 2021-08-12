7aa4e0f0103541bc54cc2df4eb51b55229e96ae3
kuma807 <kumakura013@gmail.com>
2021-08-12 16:17:03 +0900

    update 8/12

--- cpp/atcoder.cpp
+++ cpp/atcoder.cpp
@@ -12,167 +12,106 @@ template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } retu
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const double PI = 3.141592653589793;
 
-ll power = 20;
-//=============lowest_common_ancestor============================
-vector<ll> depth_lca;
-vector<vector<ll>> doubling;
-bool called_init = false;
-void init(vector<vector<ll>>& connection, ll root) {
-  depth_lca = vector<ll>(connection.size(), -1);
-  doubling = vector<vector<ll>>(power, vector<ll>(connection.size()));
-  depth_lca.at(root) = 0;
-  doubling.at(0).at(0) = -1;
-  queue<ll> q;q.push(root);
-  while (!q.empty()) {
-    ll now = q.front();q.pop();
-    ll now_d = depth_lca.at(now);
-    for (ll i = 0; i < connection.at(now).size(); ++i) {
-      if (depth_lca.at(connection.at(now).at(i)) != -1) {
-        continue;
-      }
-      depth_lca.at(connection.at(now).at(i)) = now_d + 1;
-      q.push(connection.at(now).at(i));
-      doubling.at(0).at(connection.at(now).at(i)) = now;
-    }
-  }
-  for (ll i = 0; i < power - 1; ++i) {
-    for (ll j = 0; j < connection.size(); ++j) {
-      ll temp = doubling.at(i).at(j);
-      if (temp == -1) {
-        doubling.at(i + 1).at(j) = -1;
-      }
-      else {
-        doubling.at(i + 1).at(j) = doubling.at(i).at(temp);
-      }
-    }
-  }
-}
+// ============union_find_tree===============
+struct union_find {
+  vector<ll> par;
+  vector<ll> tree_rank;
 
-ll n_up(ll now, ll n)
-{
-  for (ll i = power; i >= 0; --i) {
-    if (n & (1 << i)) {
-      now = doubling.at(i).at(now);
+  union_find(ll n)
+  {
+    par = vector<ll>(n);
+    tree_rank = vector<ll>(n);
+    for (ll i = 0; i < n; ++i)
+    {
+      par.at(i) = i;
+      tree_rank.at(i) = 0;
     }
   }
-  return now;
-}
 
-vector<ll> lowest_common_ancestor(vector<vector<ll>>& connection, ll a, ll b, ll root = 0) {
-  if (!called_init) {
-    called_init = true;
-    init(connection, root);
-  }
-  ll ancestor, distance_between = 0;
-  ll ad = depth_lca.at(a), bd = depth_lca.at(b);
-  if (ad > bd) {
-    swap(ad, bd);
-    swap(a, b);
-  }
-  b = n_up(b, bd - ad);
-  distance_between += bd - ad;
-  bd = ad;
-  ll l = -1, r = ad + 1;
-  while (l + 1 != r) {
-    ll m = (l + r) / 2;
-    ll temp = n_up(a, m);
-    if (temp == n_up(b, m)) {
-      r = m;
-      ancestor = temp;
+  ll find(ll x)
+  {
+    if (par.at(x) == x)
+    {
+      return x;
     }
-    else {
-      l = m;
+    else
+    {
+      return par.at(x) = find(par.at(x));
     }
   }
-  distance_between += r * 2;
-  return {ancestor, distance_between};
-}
-//=================================================
 
-//=============euler_tour============================
-struct euler_tour {
-  vector<ll> in, out, depth, par;
-  ll dist = 0;
-  euler_tour(vector<vector<ll>> &connection, ll s) {
-    in = out = depth = par = vector<ll>(connection.size(), -1);
-    dfs(connection, s);
-  }
-  void dfs(vector<vector<ll>> &connection, ll now) {
-    in.at(now) = dist;
-    dist += 1;
-    for (ll i = 0; i < connection.at(now).size(); ++i) {
-      ll next = connection.at(now).at(i);
-      if (next == par.at(now)) {
-        continue;
+  void unite(ll x, ll y)
+  {
+    x = find(x);
+    y = find(y);
+    if (x == y)
+    {
+      return;
+    }
+    if (tree_rank.at(x) < tree_rank.at(y))
+    {
+      par.at(x) = y;
+    }
+    else
+    {
+      par.at(y) = x;
+      if (tree_rank.at(x) == tree_rank.at(y))
+      {
+        tree_rank.at(x)++;
       }
-      depth.at(next) = depth.at(now) + 1;
-      par.at(next) = now;
-      dfs(connection, next);
     }
-    out.at(now) = dist;
-    dist += 1;
+  }
+
+  bool same(ll x, ll y)
+  {
+    return find(x) == find(y);
   }
 };
 // =======================================
+
+// ==============kruskal==================
+
+struct edge{ll u, v, cost;}; // 辺u, v をつなぐコストが cost
+bool comp(const edge& e1, const edge& e2)
+{
+  return e1.cost < e2.cost;
+}
+
+ll kruskal(vector<edge> &edges, ll V, ll E) {
+  //V 頂点、E 辺　の数
+  sort(rng(edges), comp);
+  union_find uf(V);
+  ll ans = 0;
+  for (ll i = 0; i < E; ++i)
+  {
+    edge e = edges.at(i);
+    if (!uf.same(e.u, e.v))
+    {
+      uf.unite(e.u, e.v);
+      ans += e.cost;
+    }
+  }
+  map<ll, bool> mp;
+  for (ll i = 0; i < V; ++i) {
+    mp[uf.find(i)] = true;
+  }
+  if (mp.size() != 1) {
+    return -1;
+  }
+  return ans;
+}
+// =======================================
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  ll N;
-  cin >> N;
-  vector<vector<ll>> conn(N);
-  for (ll i = 0; i < N - 1; ++i) {
-    ll a, b;
-    cin >> a >> b;
-    a -= 1, b -= 1;
-    connection.at(a).push_back(b);
-    connection.at(b).push_back(a);
+  ll N, M;
+  cin >> N >> M;
+  vector<edge> connection;
+  for (ll i = 0; i < M; ++i) {
+    ll C, L, R;
+    cin >> C >> L >> R;
+    connection.push_back({L - 1, R, C});
   }
-  euler_tour et(connection, 0);
-  init(connection, 0);
-  // for (ll i = 0; i < N; ++i) {
-  //   cout << "i" << i << "\n";
-  //   for (ll j = 0; j < connection.at(i).size(); ++j) {
-  //     cout << connection.at(i).at(j) << " ";
-  //   }
-  //   cout << "\n";
-  // }
-  // for (ll i = 0; i < N; ++i) {
-  //   cout << depth_lca.at(i) << " ";
-  // }
-  // cout << "\n";
-  // for (ll i = 0; i < N; ++i) {
-  //   cout << in.at(i) << " " << out.at(i) << "\n";
-  // }
-  // cout << "\n";
-  ll Q;
-  cin >> Q;
-  for (ll q = 0; q < Q; ++q) {
-    ll K;
-    cin >> K;
-    vector<ll> V(K);
-    for (ll i = 0; i < K; ++i) {
-      cin >> V.at(i);
-      V.at(i) -= 1;
-    }
-    ll lca = V.at(0);
-    for (ll i = 1; i < K; ++i) {
-      lca = lowest_common_ancestor(connection, lca, V.at(i)).at(0);
-    }
-    //a, node_index
-    vector<pll> node;
-    for (ll i = 0; i < K; ++i) {
-      node.push_back({et.in.at(V.at(i)), V.at(i)});
-    }
-    sort(rrng(node));
-    node.push_back({et.in.at(lca), lca});
-    ll ans = 0;
-    for (ll i = 0; i < node.size() - 1; ++i) {
-      ll a = node.at(i).first, index = node.at(i).second, b = et.out.at(index);
-      ll index2 = node.at(i + 1).second;
-      // cout << a << ' ' << b << ' ' << index << "\n";
-      ans += (depth_lca.at(index) - depth_lca.at(lowest_common_ancestor(connection, index, index2).at(0)));
-    }
-    // cout << lca << "\n";
-    cout << ans << "\n";
-  }
+  cout << kruskal(connection, N + 1, M) << "\n";
 }
--- cpp/library/graph/kruskal.cpp
+++ cpp/library/graph/kruskal.cpp
@@ -1,62 +1,67 @@
 // ============need input=================
-// MAX_N, MAX_E, V, E
+// kruskal(vector<edge> &edges, ll V, ll E)
+  //edgesは辺をすべて収納
+  //Vは頂点数
+  //Eは辺の数
 // output:
   // ll ans: 最小全域木の辺の重みの総和
 // ========================================
 
-
 // ============union_find_tree===============
-ll MAX_N = 10000;
-vector<ll> par(MAX_N);//MAX_N == num of node
-vector<ll> tree_rank(MAX_N);
+struct union_find {
+  vector<ll> par;
+  vector<ll> tree_rank;
 
-void init(ll n)
-{
-  for (ll i = 0; i < n; ++i)
+  union_find(ll n)
   {
-    par.at(i) = i;
-    tree_rank.at(i) = 0;
-  }
-}
-
-ll find(ll x)
-{
-  if (par.at(x) == x)
-  {
-    return x;
-  }
-  else
-  {
-    return par.at(x) = find(par.at(x));
-  }
-}
-
-void unite(ll x, ll y)
-{
-  x = find(x);
-  y = find(y);
-  if (x == y)
-  {
-    return;
-  }
-  if (tree_rank.at(x) < tree_rank.at(y))
-  {
-    par.at(x) = y;
-  }
-  else
-  {
-    par.at(y) = x;
-    if (tree_rank.at(x) == tree_rank.at(y))
+    par = vector<ll>(n);
+    tree_rank = vector<ll>(n);
+    for (ll i = 0; i < n; ++i)
     {
-      tree_rank.at(x)++;
+      par.at(i) = i;
+      tree_rank.at(i) = 0;
     }
   }
-}
 
-bool same(ll x, ll y)
-{
-  return find(x) == find(y);
-}
+  ll find(ll x)
+  {
+    if (par.at(x) == x)
+    {
+      return x;
+    }
+    else
+    {
+      return par.at(x) = find(par.at(x));
+    }
+  }
+
+  void unite(ll x, ll y)
+  {
+    x = find(x);
+    y = find(y);
+    if (x == y)
+    {
+      return;
+    }
+    if (tree_rank.at(x) < tree_rank.at(y))
+    {
+      par.at(x) = y;
+    }
+    else
+    {
+      par.at(y) = x;
+      if (tree_rank.at(x) == tree_rank.at(y))
+      {
+        tree_rank.at(x)++;
+      }
+    }
+  }
+
+  bool same(ll x, ll y)
+  {
+    return find(x) == find(y);
+  }
+};
 // =======================================
 
 // ==============kruskal==================
@@ -66,23 +71,27 @@ bool comp(const edge& e1, const edge& e2)
 {
   return e1.cost < e2.cost;
 }
-const ll MAX_E = 100000;
-edge es[MAX_E];
-ll V, E; // V 頂点、E 辺　の数
 
-ll kruskal()
-{
-  sort(es, es + E, comp);
-  init(V);
+ll kruskal(vector<edge> &edges, ll V, ll E) {
+  //V 頂点、E 辺　の数
+  sort(rng(edges), comp);
+  union_find uf(V);
   ll ans = 0;
   for (ll i = 0; i < E; ++i)
   {
-    edge e = es[i];
-    if (!same(e.u, e.v))
+    edge e = edges.at(i);
+    if (!uf.same(e.u, e.v))
     {
-      unite(e.u, e.v);
+      uf.unite(e.u, e.v);
       ans += e.cost;
     }
+  }
+  map<ll, bool> mp;
+  for (ll i = 0; i < V; ++i) {
+    mp[uf.find(i)] = true;
+  }
+  if (mp.size() != 1) {
+    return -1;
   }
   return ans;
 }
