85526f28cf3464c392427a2a4e97182ffca8df65
kuma807 <kumakura013@gmail.com>
2023-03-17 18:32:01 +0900

    modify rolling_has

--- cpp/library/string/rolling_hash.cpp
+++ cpp/library/string/rolling_hash.cpp
@@ -1,66 +1,147 @@
 // ============how it works=================
-// input:
-  // string a, b
-    //bを円環だとしたときa[i:i+b.size()] == b[0:b.size()]の場所を見つける
-// output:
-  //b[i]から始まる文字とaが一致するか
-  //vector<bool> res[i];res[i] から始まる文字がaと一致
+// init_rolling_hash(ll n)
+  //modの数をn個でハッシュを計算、nはデフォルトで3
+  // O(log(mod))
+
+// calc_hash(string s)
+  // sのハッシュを計算する
+  // 計算量 O(Sの長さ)
+
+// add_back(vector<pll> a, char c)
+  //文字列sのハッシュがaのときaを s + c のハッシュに置き換える
+  //計算量　O(1)
+
+// add_front(vector<pll> a, char c)
+  //文字列sのハッシュがaのときaを c + s のハッシュに置き換える
+  //計算量　O(1)
+
+// pop_back(vector<pll> a, char c)
+  //文字列sの右の文字がcの時、sから一番右の文字であるc取り除きのaを新しい文字列のハッシュに置き換える
+    // 例
+    //vector<pll> a = "abc"のハッシュ;
+    //pop_front(a, 'c')
+    //aは"ab"のハッシュになる
+  //計算量　O(1)
+
+// pop_front(vector<pll> a, char c)
+  //文字列sの左の文字がcの時、sから一番左の文字であるc取り除きのaを新しい文字列のハッシュに置き換える
+    // 例
+    //vector<pll> a = "abc"のハッシュ;
+    //pop_front(a, 'a')
+    //aは"bc"のハッシュになる
+  //計算量　O(1)
+
+//unite(vector<pll> a, vector<pll> b)
+  // 文字列a + bのハッシュを返す
+  //計算量 O(1)
 // ========================================
 
-ll M, B;
-bool init = false;
-vector<pair<ll, ll>> MB = {{2147483647, 2147483634}, {2147483629, 2147483627}, {2147483587, 2147483583}, {2147483579, 2147483576}, {2147483563, 2147483550}, {2147483543, 2147483541}, {2147483497, 2147483492}, {2147483489, 2147483486}, {2147483477, 2147483475}};
-
-//=============rolling_hash============================
-vector<bool> rolling_hash(string a, string b) {
-  if (!init) {
-    init = true;
-    random_device rnd;
-    mt19937 mt(rnd());
-    uniform_int_distribution<> rand9(0, 8);
-    ll temp = rand9(mt);
-    M = MB.at(temp).first;
-    B = MB.at(temp).second;
+//=============modinv============================
+ll modinv(ll a, ll m) {
+  ll b = m, u = 1, v = 0;
+  while (b) {
+    ll t = a / b;
+    a -= t * b; swap(a, b);
+    u -= t * v; swap(u, v);
   }
-  ll al = a.size(), bl = b.size();
-  vector<bool> res(al, false);
-  ll t = 1;
-  ll ah = 0, bh = 0;
-  for (ll i = 0; i < bl; ++i) {
-    ah = (ah * B + a.at(i)) % M;
-    bh = (bh * B + b.at(i)) % M;
-    t = (t * B) % M;
-  }
-  for (ll i = 0; i < al; ++i) {
-    if (ah == bh) {
-      res.at(i) = true;
-    }
-    ah = (ah * B + a.at((i + bl) % al)) % M;
-    ah -= (a.at(i) * t) % M;
-    if (ah < 0ll) {
-      ah += M;
-    }
-    ah %= M;
-  }
-  return res;
+  u %= m;
+  if (u < 0) u += m;
+  return u;
 }
 //=================================================
 
-
-
-
-//=========end===================
-typedef unsigned long long ull;
-const ull B = 1000000007;
 //=============rolling_hash============================
-pair<ull, ull> rolling_hash(string& a, ll len) {
-    ll al = a.size();
-    ull t = 1;
-    ull ah = 0;
-    for (ll i = 0; i < len; ++i) {
-      ah = ah * B + a[i];
-      t *= B;
+bool init_called = false;
+vector<pair<ll, ll>> MB = {{2147483647, 2147483634}, {2147483629, 2147483627}, {2147483587, 2147483583}, {2147483579, 2147483576}, {2147483563, 2147483550}, {2147483543, 2147483541}, {2147483497, 2147483492}, {2147483489, 2147483486}, {2147483477, 2147483475}};
+vector<ll> Ms, Bs, invs;
+
+void init_rolling_hash(ll n = 3) {
+  init_called = true;
+  vector<ll> already;
+  assert(n <= MB.size());
+  random_device rnd;
+  mt19937 mt(rnd());
+  for (ll i = 0; i < n; ++i) {
+    uniform_int_distribution<> rand9(0, (ll)MB.size() - i - 1);
+    ll temp = rand9(mt);
+    ll cnt = 0;
+    for (ll j = 0; j < already.size(); ++j) {
+      if (already.at(j) <= temp) {
+        cnt += 1;
+      }
     }
-    return {ah, t};
+    temp += cnt;
+    already.push_back(temp);
+    Ms.push_back(MB.at(temp).first);
+    Bs.push_back(MB.at(temp).second);
+    invs.push_back(modinv(MB.at(temp).second, MB.at(temp).first));
+  }
+  assert(already.size() == n);
+}
+
+vector<pll> calc_hash(string a) {
+  if (!init_called) {
+    init_rolling_hash();
+  }
+  vector<pll> res;
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr);
+    ll t = 1;
+    ll ah = 0;
+    for (ll i = 0; i < a.size(); ++i) {
+      ah = (ah * B + a.at(i)) % M;
+      t = (t * B) % M;
+    }
+    res.push_back({ah, t});
+  }
+  return res;
+}
+
+vector<pll> add_back(vector<pll> &a, char c) {
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr);
+    a.at(itr).first = (a.at(itr).first * B + c) % M;
+    a.at(itr).second = a.at(itr).second * B % M;
+  }
+  return a;
+}
+
+vector<pll> add_front(vector<pll> &a, char c) {
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr);
+    a.at(itr).first = (a.at(itr).first + c * a.at(itr).second) % M;
+    a.at(itr).second = a.at(itr).second * B % M;
+  }
+  return a;
+}
+
+vector<pll> pop_back(vector<pll> &a, char c) {
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr), inv = invs.at(itr);
+    a.at(itr).first = ((a.at(itr).first - c + M) % M) * inv % M;
+    a.at(itr).second = a.at(itr).second * inv % M;
+  }
+  return a;
+}
+
+vector<pll> pop_front(vector<pll> &a, char c) {
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr), inv = invs.at(itr);
+    a.at(itr).first = (a.at(itr).first - (c * a.at(itr).second % M) * inv % M + M) % M;
+    a.at(itr).second = a.at(itr).second * inv % M;
+  }
+  return a;
+}
+
+vector<pll> unite(vector<pll> a, vector<pll> b) {
+  for (ll itr = 0; itr < Ms.size(); ++itr) {
+    ll M = Ms.at(itr), B = Bs.at(itr), inv = invs.at(itr);
+    a.at(itr).first *= b.at(itr).second;
+    a.at(itr).first += b.at(itr).first;
+    a.at(itr).second *= b.at(itr).second;
+    a.at(itr).first %= M;
+    a.at(itr).second %= M;
+  }
+  return a;
 }
 //=================================================
