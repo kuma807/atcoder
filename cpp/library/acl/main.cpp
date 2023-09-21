aa616b26f139a64edaf1f3da0019feb9c358cc25
kuma807 <kumakura013@gmail.com>
2023-09-21 22:47:03 +0900

    update

--- cpp/library/acl/main.cpp
+++ cpp/library/acl/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include <atcoder/mincostflow>
+#include <atcoder/math>
 #define rng(a) a.begin(),a.end()
 #define rrng(a) a.rbegin(),a.rend()
 #define INF 2000000000000000000
@@ -11,42 +11,82 @@ using namespace atcoder;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  ll N;
-  cin >> N;
-  mcf_graph<int, ll> g(400);
-  ll team_index = 1, category_index = 151;
-  ll s = 0, t = 301;
-  for (ll i = 0; i < 150; ++i) {
-    g.add_edge(s, team_index + i, 1, 0);
+//=============is_prime============================
+bool is_prime(ll N)
+{
+  if (N == 1)
+  {
+    return false;
   }
-  for (ll i = 0; i < 150; ++i) {
-    g.add_edge(category_index + i, t, 1, 0);
-  }
-  ll MC = 1000000000;
-  for (ll i = 0; i < N; ++i) {
-    ll a, b, c;
-    cin >> a >> b >> c;
-    a = a + team_index - 1, b = b + category_index - 1;
-    g.add_edge(a, b, 1, MC - c);
-  }
-  vector<ll> ans;
-  for (ll i = 1; i <= N; ++i) {
-    mcf_graph<int, ll> temp_g = g;
-    pll f = temp_g.flow(s, t, i);
-    // cout << f.first << ' ' << f.second << "\n";
-    if (f.first != i) {
-      break;
+  for (ll i = 2; i * i <= N; ++i)
+  {
+    if (N % i == 0)
+    {
+      return false;
     }
-    ans.push_back(MC * i - f.second);
   }
-  // for (ll i = 0; i < (ll)ans.size() - 1; ++i) {
-  //   ans.at(i + 1) += ans.at(i);
+  return true;
+}
+//=================================================
+
+int main(){
+  vector<ll> P = {4, 5, 9};
+  for (ll i = 6; i <= 28; ++i) {
+    if (is_prime(i)) {
+      P.push_back(i);
+    }
+  }
+  // for (ll i = 0; i < P.size(); ++i) {
+  //   cout << 338557213 % P.at(i) << "\n";
   // }
-  cout << ans.size() << "\n";
-  for (ll i = 0; i < ans.size(); ++i) {
-    cout << ans.at(i) << "\n";
+  ll M = 0;
+  for (ll i = 0; i < P.size(); ++i) {
+    M += P.at(i);
+  }
+  cout << M << endl;
+  vector<ll> A;
+  vector<ll> len;
+  for (ll i = 0; i < P.size(); ++i) {
+    ll p = P.at(i);
+    ll s = A.size() + 1;
+    for (ll j = 0; j < p; ++j) {
+      A.push_back(s + (1 + j) % p);
+      len.push_back(p);
+    }
+  }
+  for (ll i = 0; i < A.size(); ++i) {
+    cout << A.at(i) << " ";
+  }
+  cout << endl;
+  vector<ll> B(M);
+  for (ll i = 0; i < M; ++i) {
+    cin >> B.at(i);
+  }
+  vector<pair<long long, long long>> v;
+  vector<ll> r, m;
+  for (ll i = 0; i < M; ++i) {
+    ll b = B.at(i);
+    ll l = len.at(i);
+    // if (i != M - 1 && len.at(i) == len.at(i + 1)) {
+    //   continue;
+    // }
+    if (b >= A.at(i)) {
+      // v.push_back({b - A.at(i), l});
+      r.push_back(b - A.at(i) + 1);
+      m.push_back(l);
+    }
+    else {
+      r.push_back(b + l - A.at(i) + 1);
+      m.push_back(l);
+      // v.push_back({b + l - A.at(i), l});
+    }
+  }
+
+  pll ans = crt(r, m);
+  if (ans.first == 0) {
+    cout << ans.second << "\n";
+  }
+  else {
+    cout << ans.first << endl;
   }
 }
--- cpp/temp.cpp
+++ cpp/temp.cpp
@@ -1,7 +1,50 @@
-4:r
-3:o
-0:A
-1:L
-6:T
-8:M
-AL_OR_T_M_
+#include <bits/stdc++.h>
+// #include <atcoder/modint>
+#define rng(a) a.begin(),a.end()
+#define rrng(a) a.rbegin(),a.rend()
+#define INF 2000000000000000000
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pll pair<ll, ll>
+using namespace std;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const double PI = 3.141592653589793238462643383279;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll T;
+  cin >> T;
+  for (ll testcase = 0; testcase < T; ++testcase) {
+    ll N;
+    cin >> N;
+    vector<ll> C(N);
+    multiset<pll> st;// cost, -index
+    for (ll i = 0; i < N; ++i) {
+      cin >> C.at(i);
+      st.insert({C.at(i), -(i)});
+    }
+    ll K;
+    cin >> K;
+    vector<ll> ans(N + 1, 0);
+    for (ll i = 0; i < N; ++i) {
+      pll a = *st.begin();
+      ll cost = a.first, index = -a.second;
+      ll time = K / cost;
+      ans.at(i) = time;
+      // K -= time * cost;
+      // ans.at(0) += time;
+      // ans.at(index + 1) -= time;
+      st.erase(st.find({C.at(i), -i}));
+    }
+    // for (ll i = 0; i < N; ++i) {
+    //   ans.at(i + 1) += ans.at(i);
+    // }
+    for (ll i = 0; i < N; ++i) {
+      cout << ans.at(i) << " ";
+    }
+    cout << "\n";
+  }
+}
